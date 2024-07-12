3a936366f0391e0fdfe3a1c7cfdf9791cb7c7df2
Nicolas Pannunzio <nicolas.a.pannunzio@gmail.com>
2024-07-12 16:28:21 -0300

    first commit

---
+++ src/main/java/ForoAlura/Foro2024/Foro2024Application.java
@@ -0,0 +1,13 @@
+package ForoAlura.Foro2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Foro2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Foro2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/ForoAlura/Foro2024/Foro2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package ForoAlura.Foro2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Foro2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
